e79d594636216762bfcca1c29648ced02013e6af
RUDRAKSH <119749657+RUDRAKSH-7@users.noreply.github.com>
2026-04-29 11:39:15 +0530

    Latest update to UI

--- gamelauncher.py
+++ gamelauncher.py
@@ -15,24 +15,23 @@ def run_game(game_name,game_path):
 #if the "list.bin" file contains any game, then the program will only start from menu()
 # drag and drop shortcuts or program exe, or type it directly
 # first before entering the file to list.bin, check its existence.
-
 def read_games():
     global games, game_list
     #step 1: check list.bin file existence
-    games = ""
+    games = co.INTENSE_BLUE+"║"+" "*(55)+"║\n"+co.RESET
     if Path.exists("C:/Users/Public/Documents/gamelist.bin"):
-        with open("C:/Users/Public/Documents/gamelist.bin","rb") as list:
+        with open("C:/Users/Public/Documents/gamelist.bin","rb") as list_:
             try: #checking whether THE FILE EXISTS BUT THERE IS NO DATA (RAISES EOFError)
-                game_list = pk.load(list)
+                game_list = pk.load(list_)
             except EOFError:
                 return 404 #error 404 implies that there is no element in C:/Users/Public/Documents/gamelist.bin
             
             #the game list structure will be:
             # [NAME, PATH, ICON (EMOJI)]
             for i,j in enumerate(game_list,1):
-                if i%3 == 0 and i > 1:
-                    games+="\n"
-                games += f"{[i]}  {j[2]}  {j[0]}    " #Here j[0] => game name
+                name = j[0] if len(j[0]) < 30 else j[0][:29]+"..."
+                games += f"{co.INTENSE_BLUE}║{co.RESET}           {[i]}  {j[2]}  {name}{" "*(35-len(name))}{co.INTENSE_BLUE}║{co.RESET}\n" #Here j[0] => game name
+            games += f"{co.INTENSE_BLUE}║"+" "*55+f"║\n╚═══════════════════════════════════════════════════════╝{co.RESET}"
     else:
         with open("C:/Users/Public/Documents/gamelist.bin","wb") as f:
             pass #create an empty file
@@ -42,22 +41,21 @@ def read_games():
 def add_game():
     global game_list
     sb.run(["cls"],shell=True)
-    print(rf"""{co.CYAN}
-      ▄████▄ ████▄  ████▄    ███  ██ ██████ ██     ██ 
+    print(rf"""{co.BLUE}      ▄████▄ ████▄  ████▄    ███  ██ ██████ ██     ██ 
       ██▄▄██ ██  ██ ██  ██   ██ ▀▄██ ██▄▄   ██ ▄█▄ ██ 
       ██  ██ ████▀  ████▀    ██   ██ ██▄▄▄▄  ▀██▀██▀
 {co.RESET}""",end="")
-    game_path = str(input("* Drag and drop the game exe\n  or the shortcut file Here\n\n  >> "))
+    game_path = str(input("      * Drag and drop the game exe\n        or the shortcut file Here\n\n  >> "))
     game_path=game_path.strip('\"')
     if not Path.exists(game_path.strip('\"')):
-        print("\n  Error :: The provided game path doesn't seem to exist...\n  RETURNING TO MAIN SCREEN",end="")
+        print(f"\n  Error :: The provided game path doesn't seem to exist...\n  {co.INTENSE_RED}RETURNING TO MAIN SCREEN{co.RESET}",end="")
         time.sleep(2)
 
         return 1
     
     game_name = str(input("\n* Enter Game name\n  >> "))
     if len(game_name) == 0:
-        print("\n  Error :: Game name can not be left empty...\n  RETURNING TO MAIN SCREEN",end="")
+        print(f"\n  Error :: Game name can not be left empty...\n  {co.INTENSE_RED}RETURNING TO MAIN SCREEN{co.RESET}",end="")
         time.sleep(2)
 
         return 1
@@ -69,8 +67,8 @@ def add_game():
 
     #adding game list.bin (truncating the whole file, entering the games_list at once)
     game_list.append([game_name,game_path,icon])
-    with open("C:/Users/Public/Documents/gamelist.bin","wb") as list:
-        pk.dump(game_list,list)
+    with open("C:/Users/Public/Documents/gamelist.bin","wb") as list_:
+        pk.dump(game_list,list_)
     time.sleep(2)
 
     return 0
@@ -82,8 +80,8 @@ def trash_game():
         del game_list[game_index-1]
         print(f"{co.RED}\n  GAME REMOVED...\n{co.RESET}")
 
-        with open("C:/Users/Public/Documents/gamelist.bin","wb") as list:
-            pk.dump(game_list,list)
+        with open("C:/Users/Public/Documents/gamelist.bin","wb") as list_:
+            pk.dump(game_list,list_)
 
         time.sleep(2)
         return 0
@@ -95,22 +93,23 @@ def trash_game():
 def menu():
     global games
     sb.run(["cls"],shell=True)
-    print(rf"""              
-       ██████╗  █████╗ ███╗   ███╗███████╗███████╗
-      ██╔════╝ ██╔══██╗████╗ ████║██╔════╝██╔════╝
-      ██║  ███╗███████║██╔████╔██║█████╗  ███████╗
-      ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝  ╚════██║
-      ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗███████║
-       ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝                             
- {co.RESET}""",end="")
-    print(f"{co.INTENSE_CYAN}═════════ [+] ADD GAME ══════ [-] REMOVE GAME ═════════{co.RESET}")
+    print(rf"{co.INTENSE_BLUE}╔═══════════════════════════════════════════════════════╗{co.RESET}")
+    print(rf"""{co.INTENSE_BLUE}║{co.RESET}      ██████╗  █████╗ ███╗   ███╗███████╗███████╗      {co.INTENSE_BLUE}║{co.RESET}
+{co.INTENSE_BLUE}║{co.RESET}     ██╔════╝ ██╔══██╗████╗ ████║██╔════╝██╔════╝      {co.INTENSE_BLUE}║{co.RESET}
+{co.INTENSE_BLUE}║{co.RESET}     ██║  ███╗███████║██╔████╔██║█████╗  ███████╗      {co.INTENSE_BLUE}║{co.RESET}
+{co.INTENSE_BLUE}║{co.RESET}     ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝  ╚════██║      {co.INTENSE_BLUE}║{co.RESET}
+{co.INTENSE_BLUE}║{co.RESET}     ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗███████║      {co.INTENSE_BLUE}║{co.RESET}
+{co.INTENSE_BLUE}║{co.RESET}      ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝      {co.INTENSE_BLUE}║{co.RESET}
+{co.RESET}""",end="")
+    print(f"{co.INTENSE_BLUE}╠═════════ {co.INTENSE_GREEN}[+] ADD GAME{co.INTENSE_BLUE} ══════ {co.INTENSE_RED}[-] REMOVE GAME{co.INTENSE_BLUE} ═════════╣{co.RESET}")
     print(games)
 
-read_games()
+if read_games() == 1:
+    add_game()
 while True:
     menu()
     try:
-        choice = str(input(f"{co.CYAN}  >> "))[0] ; print(co.RESET)
+        choice = str(input(f"{co.BLUE}  >> "))[0] ; print(co.RESET)
     except:
         print(co.RESET)
         continue
@@ -120,7 +119,7 @@ while True:
         game_index = int(choice)-1
         if game_index in range(0,len(game_list)):
             run_game(game_list[game_index][0],game_list[game_index][1])
-            time.sleep(3)
+            time.sleep(1)
             continue
     except:
         pass
